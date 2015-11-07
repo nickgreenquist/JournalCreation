a581ea810a0c609fb3b871d91a16b620511fa5c6
nickgreenquist <nickgreenquist@gmail.com>
2015-11-07 16:17:05 -0500

    CombineFiles now will merge all word docs in a folder (and its subfolders)

--- BatchProgram/JournalCreation.bat
+++ BatchProgram/JournalCreation.bat
@@ -7,18 +7,18 @@ for /F "tokens=2,3 delims=/ " %%A in ('echo %CDATE%') do set yyyy=%%B
 set dateNew=%mm%-%dd%-%yyyy%
 
 set month-num=%date:~3,2%
-if %mm%==01 set mo-name=January
-if %mm%==02 set mo-name=February
-if %mm%==03 set mo-name=March
-if %mm%==04 set mo-name=April
-if %mm%==05 set mo-name=May
-if %mm%==06 set mo-name=June
-if %mm%==07 set mo-name=July
-if %mm%==08 set mo-name=August
-if %mm%==09 set mo-name=September
-if %mm%==10 set mo-name=October
-if %mm%==11 set mo-name=November
-if %mm%==12 set mo-name=December
+if %mm%==01 set mo-name=01-January
+if %mm%==02 set mo-name=02-February
+if %mm%==03 set mo-name=03-March
+if %mm%==04 set mo-name=04-April
+if %mm%==05 set mo-name=05-May
+if %mm%==06 set mo-name=06-June
+if %mm%==07 set mo-name=07-July
+if %mm%==08 set mo-name=08-August
+if %mm%==09 set mo-name=09-September
+if %mm%==10 set mo-name=10-October
+if %mm%==11 set mo-name=11-November
+if %mm%==12 set mo-name=12-December
 
 set path=D:\Personal\Journal\%yyyy%\%mo-name%\%dateNew%.docx
 set monthPath=D:\Personal\Journal\%yyyy%\%mo-name%
--- JournalProcessing/CombineFiles/Form1.cs
+++ JournalProcessing/CombineFiles/Form1.cs
@@ -7,14 +7,125 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using DocumentFormat.OpenXml.Packaging;
+using System.IO.Packaging;
+using System.Xml;
+using System.Text.RegularExpressions;
+using RasterEdge.Imaging.Basic;
+using RasterEdge.XDoc.Word;
 
 namespace CombineFiles
 {
     public partial class Form1 : Form
     {
+        public static string AllText;
+        public static List<string> OrderedListOfFiles;
+
         public Form1()
         {
             InitializeComponent();
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+            AllText = "";
+            OrderedListOfFiles = new List<string>();
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] foldersArray = (string[])e.Data.GetData(DataFormats.FileDrop);
+            List<string> folders = foldersArray.ToList();
+            folders.Sort();
+
+            //keep getting all subfolders until we hit files
+            foreach(string folder in foldersArray)
+            {
+                Console.WriteLine(folder);
+                GetSubFolders(folder);
+            }
+
+            DOCXDocument.CombineDocument(OrderedListOfFiles.ToArray(), folders[0] + "test.docx");
+
+            //reset lists
+            OrderedListOfFiles.Clear();
+            AllText = "";
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+        
+        private void GetSubFolders(string path)
+        {
+            string[] foldersArray = Directory.GetDirectories(path);
+            List<string> folders = foldersArray.ToList();
+            folders.Sort();
+            foreach (string folder in folders)
+            {
+                GetSubFolders(folder);
+            }
+
+            //once we have dealt with any subfolders, we add the files we find in the folder
+            string[] filesInfolder = Directory.GetFiles(path);
+            List<string> files = filesInfolder.ToList();
+            files.Sort();
+            foreach (string file in files)
+            {
+                string ext = Path.GetExtension(file);
+                if (ext.Equals(".docx") || ext.Equals(".doc"))
+                {
+                    try
+                    {
+                        TryToOpenWord(file);
+                        OrderedListOfFiles.Add(file);
+                    }
+                    catch
+                    {
+                        //if this fails, this is not a valid word doc
+                    }
+                }
+            }
+        }
+
+        private void TryToOpenWord(string file)
+        {
+            using (WordprocessingDocument wdDoc = WordprocessingDocument.Open(file, false))
+            {
+            }
+        }
+
+        private string TextFromWord(string file)
+        {
+            const string wordmlNamespace = "http://schemas.openxmlformats.org/wordprocessingml/2006/main";
+
+            StringBuilder textBuilder = new StringBuilder();
+                using (WordprocessingDocument wdDoc = WordprocessingDocument.Open(file, false))
+                {
+                    // Manage namespaces to perform XPath queries.  
+                    NameTable nt = new NameTable();
+                    XmlNamespaceManager nsManager = new XmlNamespaceManager(nt);
+                    nsManager.AddNamespace("w", wordmlNamespace);
+
+                    // Get the document part from the package.  
+                    // Load the XML in the document part into an XmlDocument instance.  
+                    XmlDocument xdoc = new XmlDocument(nt);
+                    xdoc.Load(wdDoc.MainDocumentPart.GetStream());
+
+                    XmlNodeList paragraphNodes = xdoc.SelectNodes("//w:p", nsManager);
+                    foreach (XmlNode paragraphNode in paragraphNodes)
+                    {
+                        XmlNodeList textNodes = paragraphNode.SelectNodes(".//w:t", nsManager);
+                        foreach (System.Xml.XmlNode textNode in textNodes)
+                        {
+                            textBuilder.Append(textNode.InnerText);
+                        }
+                        textBuilder.Append(Environment.NewLine);
+                    }
+
+                }
+            return textBuilder.ToString();
         }
     }
 }
